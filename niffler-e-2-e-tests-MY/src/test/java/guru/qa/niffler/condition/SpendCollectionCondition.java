cbe40f836d61f3bd7052c4f50959d6d118677fc0
asham <a.shamsutdinov2021@gmail.com>
2024-03-10 16:59:23 +0300

    homework 12

---
+++ niffler-e-2-e-tests-MY/src/test/java/guru/qa/niffler/condition/SpendCollectionCondition.java
@@ -0,0 +1,91 @@
+package guru.qa.niffler.condition;
+
+import com.codeborne.selenide.CheckResult;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Driver;
+import com.codeborne.selenide.ex.ElementNotFound;
+import com.codeborne.selenide.ex.TextsMismatch;
+import com.codeborne.selenide.impl.CollectionSource;
+import guru.qa.niffler.model.spend.SpendJson;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpendCollectionCondition {
+
+    public static CollectionCondition spends(SpendJson... expectedSPends) {
+        return new CollectionCondition() {
+
+            private List<String> expectedTexts;
+
+            @Override
+            public void fail(CollectionSource collection, CheckResult lastCheckResult, @Nullable Exception cause, long timeoutMs) {
+                List<String> actualTexts = lastCheckResult.getActualValue();
+                if (actualTexts == null || actualTexts.isEmpty()) {
+                    throw new ElementNotFound(collection, toString(), timeoutMs, cause);
+                }
+                String message = lastCheckResult.getMessageOrElse(() -> "Texts mismatch");
+                throw new TextsMismatch(message, collection, expectedTexts, actualTexts, explanation, timeoutMs, cause);
+            }
+
+            @Nonnull
+            @Override
+            public CheckResult check(Driver driver, List<WebElement> elements) {
+                expectedTexts = new ArrayList<>();
+                if (elements.size() != expectedSPends.length) {
+                    return CheckResult.rejected("Incorrect table size", elements);
+                }
+                boolean checkPassed = true;
+                List<String> spendRows = new ArrayList<>();
+                for (int i = 0; i < expectedSPends.length; i++) {
+                    WebElement row = elements.get(i);
+                    SpendJson expectedSpending = expectedSPends[i];
+                    List<WebElement> cells = row.findElements(By.cssSelector("td"));
+
+                    spendRows.add("amount: " + cells.get(2).getText() +
+                            ", currency: " + cells.get(3).getText() +
+                            ", category: " + cells.get(4).getText() +
+                            ", description: " + cells.get(5).getText());
+
+                    expectedTexts.add("amount: " + expectedSpending.amount() +
+                            ", currency: " + expectedSpending.currency().name() +
+                            ", category: " + expectedSpending.category() +
+                            ", description: " + expectedSpending.description());
+
+                    if (!Double.valueOf(cells.get(2).getText()).equals(expectedSpending.amount())) {
+                        checkPassed = false;
+                        break;
+                    }
+                    if (!cells.get(3).getText().equals(expectedSpending.currency().name())) {
+                        checkPassed = false;
+                        break;
+                    }
+                    if (!cells.get(4).getText().equals(expectedSpending.category())) {
+                        checkPassed = false;
+                        break;
+                    }
+                    if (!cells.get(5).getText().equals(expectedSpending.description())) {
+                        checkPassed = false;
+                        break;
+                    }
+                }
+
+                if (checkPassed) {
+                    return CheckResult.accepted();
+                } else {
+                    return CheckResult.rejected("Incorrect spends content", spendRows);
+                }
+            }
+
+            @Override
+            public boolean missingElementSatisfiesCondition() {
+                return false;
+            }
+        };
+
+    }
+}
--- niffler-e-2-e-tests-MY/src/test/java/guru/qa/niffler/page/ProfilePage.java
+++ niffler-e-2-e-tests-MY/src/test/java/guru/qa/niffler/page/ProfilePage.java
@@ -1,73 +1,24 @@
 package guru.qa.niffler.page;
 
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import io.qameta.allure.Step;
+import guru.qa.niffler.page.message.SuccessMsg;
 
-import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class ProfilePage extends BasePage<ProfilePage> {
 
-    private final SelenideElement editAvatarBtn = $("button.profile__avatar-edit");
-    private final SelenideElement uploadAvatar = $(".edit-avatar__input[type=file]");
-    private final SelenideElement avatar = $("img.profile__avatar");
-    private final SelenideElement firstname = $("[name=firstname]");
-    private final SelenideElement surname = $("[name=surname]");
-    private final SelenideElement submitBtn = $("button[type=submit]");
-    private final SelenideElement category = $("[name=category]");
-    private final SelenideElement addCategoryBtn = $(".add-category__input-container button");
-    private final ElementsCollection categories = $$(".categories__list .categories__item");
+    public static final String PAGE_URL = CFG.frontUrl() + "/profile";
 
+    private final SelenideElement avatarInput = $("input[type='file']");
+    private final SelenideElement avatar = $(".profile__avatar");
+    private final SelenideElement submitBtn = $("button[type='submit']");
 
-
-    @Step("Загрузить аватар")
-    public ProfilePage uploadAvatarFromClasspath(String classpath) {
-        editAvatarBtn.click();
-        uploadAvatar.uploadFromClasspath(classpath);
+    public ProfilePage addAvatar(String imagePath) {
+        avatar.click();
+        avatarInput.uploadFromClasspath(imagePath);
+        submitBtn.click();
+        checkMessage(SuccessMsg.PROFILE_MSG);
         return this;
     }
 
-    @Step("Указать имя пользователя '{firstname}'")
-    public ProfilePage setFirstname(String firstname) {
-        this.firstname.setValue(firstname);
-        return this;
-    }
-
-    @Step("Указать фамилию пользователя '{surname}'")
-    public ProfilePage setSurname(String surname) {
-        this.surname.setValue(surname);
-        return this;
-    }
-
-    @Step("Подтвердить ввод пользовательских данных")
-    public ProfilePage submitData() {
-        submitBtn.scrollTo().click();
-        return this;
-    }
-
-    @Step("Выбирать категорию '{category}'")
-    public ProfilePage setCategory(String category) {
-        this.category.setValue(category);
-        return this;
-    }
-
-    @Step("Новая категория")
-    public ProfilePage addCategory() {
-        addCategoryBtn.click();
-        return this;
-    }
-
-    @Step("Убедиться, что указано имя '{firstname}'")
-    public ProfilePage checkFirstname(String firstname) {
-        this.firstname.shouldHave(value(firstname));
-        return this;
-    }
-
-    @Step("Убедиться, что указана фамилия '{surname}'")
-    public ProfilePage checkSurname(String surname) {
-        this.surname.shouldHave(value(surname));
-        return this;
-    }
 }
