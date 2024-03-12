445aafdf8e19950fa9aa91b4ed25fd3c21ff6625
asham <a.shamsutdinov2021@gmail.com>
2024-03-12 21:26:48 +0300

    Oauth 14

--- niffler-e-2-e-tests-MY/src/test/java/guru/qa/niffler/jupiter/annotation/DbUser.java
+++ niffler-e-2-e-tests-MY/src/test/java/guru/qa/niffler/jupiter/annotation/DbUser.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@ExtendWith(DbUserExtension.class)
+@ExtendWith(CreateUserExtension.class)
 public @interface DbUser {
 
     String userName() default "";
